16940af3967e5ccab0aee6903fd096d2d0b00621
Santiago Camacho <38534363+santiago007-96@users.noreply.github.com>
2023-05-23 10:55:30 -0500

    Diseño de base datos, diagramas y generación de scripts
    Primeramente se realizó el diseño de los diagramas de caso de uso, posterior a ello los diagramas entidad-relación y los scripts de los mismos. También se empezó a desarrollar el Backend de la aplicación.

---
+++ CODE/03. BBDD/01. GAS/GR04_GAS_BBDD.sql
@@ -0,0 +1,31 @@
+/*==============================================================*/
+/* DBMS name:      Microsoft SQL Server 2014                    */
+/* Created on:     22/5/2023 18:27:44                           */
+/*==============================================================*/
+
+
+if exists (select 1
+            from  sysobjects
+           where  id = object_id('USUARIO')
+            and   type = 'U')
+   drop table USUARIO
+go
+
+/*==============================================================*/
+/* Table: USUARIO                                               */
+/*==============================================================*/
+create table USUARIO (
+   USU_CODIGO           numeric              identity,
+   USU_NOMBRE           varchar(50)          not null,
+   USU_APELLIDO         varchar(50)          not null,
+   USU_CEDULA           varchar(10)          not null,
+   USU_DIRECCION        varchar(100)         not null,
+   USU_TELEFONO         varchar(20)          not null,
+   USU_EMAIL            varchar(100)         not null,
+   USU_USUARIO          varchar(100)         not null,
+   USU_PASSWORD         varchar(100)         not null,
+   USU_ISADMIN          bit                  not null,
+   constraint PK_USUARIO primary key (USU_CODIGO)
+)
+go
+
---
+++ CODE/03. BBDD/02. COREGAS/GR04_COREGAS_BBDD.sql
@@ -0,0 +1,81 @@
+/*==============================================================*/
+/* DBMS name:      Microsoft SQL Server 2014                    */
+/* Created on:     22/5/2023 20:37:38                           */
+/*==============================================================*/
+
+
+if exists (select 1
+   from sys.sysreferences r join sys.sysobjects o on (o.id = r.constid and o.type = 'F')
+   where r.fkeyid = object_id('FACTURA') and o.name = 'FK_FACTURA_CLIENTE_F_CLIENTE')
+alter table FACTURA
+   drop constraint FK_FACTURA_CLIENTE_F_CLIENTE
+go
+
+if exists (select 1
+            from  sysobjects
+           where  id = object_id('CLIENTE')
+            and   type = 'U')
+   drop table CLIENTE
+go
+
+if exists (select 1
+            from  sysindexes
+           where  id    = object_id('FACTURA')
+            and   name  = 'CLIENTE_FACTURA_FK'
+            and   indid > 0
+            and   indid < 255)
+   drop index FACTURA.CLIENTE_FACTURA_FK
+go
+
+if exists (select 1
+            from  sysobjects
+           where  id = object_id('FACTURA')
+            and   type = 'U')
+   drop table FACTURA
+go
+
+/*==============================================================*/
+/* Table: CLIENTE                                               */
+/*==============================================================*/
+create table CLIENTE (
+   CLI_CODIGO           numeric              identity,
+   CLI_NOMBRE           varchar(50)          not null,
+   CLI_APELLIDO         varchar(50)          not null,
+   CLI_ID               varchar(16)          not null,
+   CLI_EMAIL            varchar(50)          not null,
+   CLI_TEL              varchar(10)          not null,
+   CLI_DIRECCION        varchar(100)         not null,
+   CLI_BORRADO          bit                  not null,
+   constraint PK_CLIENTE primary key (CLI_CODIGO)
+)
+go
+
+/*==============================================================*/
+/* Table: FACTURA                                               */
+/*==============================================================*/
+create table FACTURA (
+   FACT_CODIGO          numeric              identity,
+   CLI_CODIGO           numeric              not null,
+   FACT_NUMERO          varchar(50)          not null,
+   FACT_FECHA           datetime             not null,
+   FACT_MONTOTOTAL      decimal(10,2)        not null,
+   FACT_BORRADO         bit                  not null,
+   constraint PK_FACTURA primary key (FACT_CODIGO)
+)
+go
+
+/*==============================================================*/
+/* Index: CLIENTE_FACTURA_FK                                    */
+/*==============================================================*/
+
+
+
+
+create nonclustered index CLIENTE_FACTURA_FK on FACTURA (CLI_CODIGO ASC)
+go
+
+alter table FACTURA
+   add constraint FK_FACTURA_CLIENTE_F_CLIENTE foreign key (CLI_CODIGO)
+      references CLIENTE (CLI_CODIGO)
+go
+

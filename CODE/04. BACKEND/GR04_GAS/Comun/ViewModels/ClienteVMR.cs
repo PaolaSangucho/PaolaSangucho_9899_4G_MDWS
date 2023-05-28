472c7ae82e795fd4d6015966bb49b6e3b7697a15
Santiago Camacho <38534363+santiago007-96@users.noreply.github.com>
2023-05-28 14:46:43 -0500

    Acceso a Datos

---
+++ CODE/04. BACKEND/GR04_GAS/Comun/ViewModels/ClienteVMR.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Comun.ViewModels
+{
+    public class ClienteVMR
+    {
+        public long CLI_CODIGO { get; set; }
+        public string CLI_NOMBRE { get; set; }
+        public string CLI_APELLIDO { get; set; }
+        public string CLI_ID { get; set; }
+        public string CLI_EMAIL { get; set; }
+        public string CLI_TEL { get; set; }
+        public string CLI_DIRECCION { get; set; }
+    }
+}
---
+++ CODE/04. BACKEND/GR04_GAS/Datos/DAL/ClienteDAL.cs
@@ -0,0 +1,44 @@
+﻿using Comun.ViewModels;
+using Modelo.Modelos;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Datos.DAL
+{
+    public class ClienteDAL
+    {
+        public static ListadoPaginadoVMR<ClienteVMR> Gets(int cantidad, int pagina, string textoBusqueda)
+        {
+            ListadoPaginadoVMR<ClienteVMR> resultado = new ListadoPaginadoVMR<ClienteVMR>();
+            //
+
+            return resultado;
+        }
+        public static ClienteVMR Get(long id)
+        {
+            ClienteVMR item = null;
+            //
+
+            return item;
+        }
+        //Devuelve el PK el elemento creado
+        public static long Post(Cliente item)
+        {
+            long id = 0;
+            //
+            return id;
+        }
+        public static void Put(ClienteVMR item)
+        {
+            //
+        }
+        public static void Delete(List<long> ids)
+        {
+            //
+        }
+    }
+}

4fa13f7a42c7a3fdb6629664fe60ae14de9ddb35
Santiago Camacho <38534363+santiago007-96@users.noreply.github.com>
2023-06-04 17:32:43 -0500

    Acceso a Datos

--- CODE/04. BACKEND/GR04_GAS/Datos/DAL/UsuarioDAL.cs
+++ CODE/04. BACKEND/GR04_GAS/Datos/DAL/UsuarioDAL.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Comun.ViewModels;
+using Modelo.Modelos;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,25 @@ using System.Threading.Tasks;
 
 namespace Datos.DAL
 {
-    internal class UsuarioDAL
+    public class UsuarioDAL
     {
+        public static UsuarioVMR Get(string user, string password)
+        {
+            UsuarioVMR item = null;
+
+            using (var db = DbConexion.Create())
+            {
+                item = db.USUARIO.Where(u => user.Contains(u.USU_USUARIO) && password.Contains(u.USU_PASSWORD)).Select(u => new UsuarioVMR
+                {
+                    nombre = u.USU_NOMBRE,
+                    apellido = u.USU_APELLIDO,
+                    email = u.USU_EMAIL,
+                    usuario = u.USU_USUARIO,
+                    contrasenia = u.USU_PASSWORD
+                }).FirstOrDefault();
+            }
+
+            return item;
+        }
     }
 }

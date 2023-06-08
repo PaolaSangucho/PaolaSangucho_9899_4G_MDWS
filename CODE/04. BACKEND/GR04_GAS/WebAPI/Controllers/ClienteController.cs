1982b0eb77e498a62158ff09e4217931965f70fb
Santiago Camacho <38534363+santiago007-96@users.noreply.github.com>
2023-06-07 21:10:47 -0500

    Web API

--- CODE/04. BACKEND/GR04_GAS/WebAPI/Controllers/ClienteController.cs
+++ CODE/04. BACKEND/GR04_GAS/WebAPI/Controllers/ClienteController.cs
@@ -7,9 +7,11 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using System.Web.Http.Cors;
 
 namespace WebAPI.Controllers
 {
+    [EnableCors(origins: "*", headers: "*", methods: "*")]
     public class ClienteController : ApiController
     {
         public IHttpActionResult Gets(int cantidad = 10, int pagina = 0, string textoBusqueda = null)
@@ -30,9 +32,30 @@ namespace WebAPI.Controllers
             return Content(respuesta.codigo, respuesta);
         }
 
+        [HttpGet]
         public IHttpActionResult Get(long codigo)
         {
+            var respuesta = new RespuestaVMR<ClienteVMR>();
 
+            try
+            {
+                respuesta.datos = ClienteBLL.Get(codigo);
+            }
+            catch (Exception ex)
+            {
+                respuesta.codigo = HttpStatusCode.InternalServerError;
+                respuesta.datos = null;
+                respuesta.mensajes.Add(ex.Message);
+                respuesta.mensajes.Add(ex.ToString());
+            }
+
+            if(respuesta.datos == null && respuesta.mensajes.Count() == 0)
+            {
+                respuesta.codigo = HttpStatusCode.NotFound;
+                respuesta.mensajes.Add("Elemento no encontrado!");
+            }
+
+            return Content(respuesta.codigo, respuesta);
         }
 
         public IHttpActionResult Post(CLIENTE item)

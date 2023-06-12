187f1ea7152e6f3dd1b25eb73af63d8e741edd04
Santiago Camacho <38534363+santiago007-96@users.noreply.github.com>
2023-06-12 08:11:22 -0500

    primer sprint

---
+++ CODE/04. BACKEND/GR04_GAS/Datos/DAL/UsuarioDAL.cs
@@ -0,0 +1,33 @@
+﻿using Comun.ViewModels;
+using Modelo.Modelos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Datos.DAL
+{
+    public class UsuarioDAL
+    {
+        public static UsuarioVMR LeerUno(string user, string password)
+        {
+            UsuarioVMR item = null;
+
+            using (var db = DbConexion.Create())
+            {
+                item = db.USUARIO.Where(u => user.Contains(u.USU_USUARIO) && password.Contains(u.USU_PASSWORD)).Select(u => new UsuarioVMR
+                {
+                    USU_NOMBRE = u.USU_NOMBRE,
+                    USU_APELLIDO = u.USU_APELLIDO,
+                    USU_EMAIL = u.USU_EMAIL,
+                    USU_USUARIO = u.USU_USUARIO,
+                    USU_PASSWORD = u.USU_PASSWORD,
+                    USU_ISADMIN = u.USU_ISADMIN
+                }).FirstOrDefault();
+            }
+
+            return item;
+        }
+    }
+}
---
+++ CODE/04. BACKEND/GR04_GAS/WebAPI/Controllers/UsuarioController.cs
@@ -0,0 +1,72 @@
+﻿using Comun.ViewModels;
+using Logica.BLL;
+using Modelo.Modelos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Cors;
+
+namespace WebAPI.Controllers
+{
+    [EnableCors(origins: "*", headers: "*", methods: "*")]
+    public class UsuarioController : ApiController
+    {
+        [HttpGet]
+        public IHttpActionResult LeerUno(string user, string password)
+        {
+            var respuesta = new RespuestaVMR<UsuarioVMR>();
+
+            try
+            {
+                respuesta.datos = UsuarioBLL.LeerUno(user, password);
+            }
+            catch (Exception ex)
+            {
+                respuesta.codigo = HttpStatusCode.InternalServerError;
+                respuesta.datos = null;
+                respuesta.mensajes.Add(ex.Message);
+                respuesta.mensajes.Add(ex.ToString());
+            }
+
+            if (respuesta.datos == null && respuesta.mensajes.Count() == 0)
+            {
+                respuesta.codigo = HttpStatusCode.NotFound;
+                respuesta.mensajes.Add("Usuario no encontrado!");
+            }
+
+            return Content(respuesta.codigo, respuesta);
+        }
+
+        
+        [HttpPost]
+        [Route("api/usuario/{user}/{password}")]
+        public IHttpActionResult Login(string user, string password)
+        {
+            var respuesta = new RespuestaVMR<UsuarioVMR>();
+            var userAux = user;
+            var passwordAux = password;
+            try
+            {
+                respuesta.datos = UsuarioBLL.LeerUno(user, password);
+            }
+            catch (Exception ex)
+            {
+                respuesta.codigo = HttpStatusCode.InternalServerError;
+                respuesta.datos = null;
+                respuesta.mensajes.Add(ex.Message);
+                respuesta.mensajes.Add(ex.ToString());
+            }
+
+            if (respuesta.datos == null && respuesta.mensajes.Count() == 0)
+            {
+                respuesta.codigo = HttpStatusCode.NotFound;
+                respuesta.mensajes.Add("Usuario no encontrado!");
+            }
+
+            return Content(respuesta.codigo, respuesta);
+        }
+    }
+}
